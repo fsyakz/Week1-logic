a8b936ccb81db0d4843fdf3a205bbbb57eb3c0bd
fsyakz <fasyaalghoniyyu47@gmail.com>
2025-03-20 14:14:00 +0700

    upload pertama kali

---
+++ syntax1.js
@@ -0,0 +1,33 @@
+//operasi aritmatika
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2, 'pertambahan');
+console.log(angka1 - angka2, 'pengurangan');
+console.log(angka1 * angka2, 'perkalian');
+console.log(angka1 / angka2, 'pembagian');
+console.log(angka1 % angka2, 'modulus');
+
+//disini kita belajar mengisi variable
+
+let PenampungHasilOperasiAritmatika = angka1 + angka2;
+console.log(PenampungHasilOperasiAritmatika);
+
+PenampungHasilOperasiAritmatika = angka1 - angka2;
+console.log(PenampungHasilOperasiAritmatika);
+
+PenampungHasilOperasiAritmatika = angka1 * angka2;
+console.log(PenampungHasilOperasiAritmatika);
+
+PenampungHasilOperasiAritmatika = angka1 / angka2;
+console.log(PenampungHasilOperasiAritmatika);
+
+PenampungHasilOperasiAritmatika = angka1 % angka2;
+console.log(PenampungHasilOperasiAritmatika);
+
+//string juga bisa ditambah namun string ga bisa dijaddikan seperti operator aritmatika 
+let firstname = "fasya";
+let lastname = "alghoniyyu";
+let fullname = firstname + lastname;
+
+console.log(fullname)
